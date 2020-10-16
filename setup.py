9fe7086c91655e5fec362df2f492785fde5e8888
GabrielBlancoM <gaboblanco25@icloud.com>
2020-10-15 21:15:00 -0600

    'copia'

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@
 from setuptools import setup
 
 setup(
-    name="Package_example1", # Replace with your own username
-    version="0.0.1",
+    name='Package_example1',
+    version='0.0.1',
     author="GabrielBlancoMora",
+    description='Paquete para tarea 3',
     author_email="gaboblanco256@gmail.com",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/GabrielBlancoM/Tarea3V2.0.git",
+    license='unlicense',
+    #package_dir={'':'Package_example1'},
+    #scripts=['Package_example2/images.py', 'Package_example2/sound.py','Pacage_example2/text.py'],
+    url="https://github.com/GabrielBlancoM/Tarea3V2.0",
     packages=['Package_example1'],
-    zip_safe=False,
-    )
+    zip_safe=False
+ 
+ )
